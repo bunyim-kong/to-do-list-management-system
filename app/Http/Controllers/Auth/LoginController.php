cf349a4025edd407e412bd3272d6041cb02bfcb1
vannararin23-sketch <vannararin23@gmail.com>
2026-05-07 14:24:52 +0700

    feat:

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Controllers\Auth;
 
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
+use Ap
 
 class LoginController extends Controller
 {
     //
-    public function showLogin()
+    public function index()
     {
-        return view('pages.auth.login');
+        $login = login::all();
+        return view('form', compact('form'));
+
+    }
+    public function create()
+    {
+        return view('form');
+    }
+    public function store(Request $request)
+    {
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $gender = $request->input('gender');
     }
 }
--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
 </head>
-<body class="bg-[#948989] flex justify-center items-center">
-    <div class="w-full flex justify-center items-center h-screen ">
-        <div class=" p-6 shadow-lg bg-[#FFFFFF] rounded-2xl">
-            <h1>Welcome Back </h1>
+<body class="bg-gray-100 flex justify-center items-center min-h-screen">
+
+    <div class="bg-white p-8 rounded-2xl shadow-lg w-full max-w-2xl">
+        <div>
+            <h1 class="text-red-500">Welcome Back </h1>
             <span>Let’s get started with your 30 days free trail</span>
         </div>
         <form>
