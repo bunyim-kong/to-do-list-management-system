afb2b192b1b21033e10743760963eefaee59a0de
Seyhaz-Dev <seyhogaa@gmail.com>
2026-05-07 16:30:41 +0700

    not yet

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Dashboard;
-use Illuminate\Http\Request;
-use ReturnTypeWillChange;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        $totalTasks = Dashboard::count();
+
+       $completedTasks = Dashboard::where('completed_tasks', 'completed')->count();
+       
+
+        $pendingTasks = Dashboard::where('completed_tasks', 'pending')->count();
+
+        $tasks = Dashboard::latest()->get();
+
+        return view('dashboard.index', compact(
+            
+            'totalTasks',
+            'completedTasks',
+            'pendingTasks',
+            'tasks'
+        ));
     }
 }
